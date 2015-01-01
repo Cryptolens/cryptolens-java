78fc2d32a44843445c6e018b9451ed337bffab2b
artemlos <artem@artemlos.net>
2015-01-01 23:24:24 +0100

    Revert "a"
    This reverts commit cd0770a13226a1306be1618e6cc6748c9bc45228.

--- SKGL Extension/src/test/SKMTest.java
+++ SKGL Extension/src/test/SKMTest.java
@@ -2,12 +2,7 @@ package test;
 
 import static org.junit.Assert.*;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 
 import skgl.*;
@@ -34,23 +29,6 @@ public class SKMTest {
 			System.out.println(output.get("error"));
 			fail(output.get("error"));
 		}
-	}
-	
-	@Test
-	public void  GetKeyInformationFromParametersTest() throws NumberFormatException, ParseException {
-		
-		Map<String,String> input= new HashMap<String, String>();
-		
-		input.put("uid", "2");
-        input.put("pid", "3");
-        input.put("hsum", "751963");
-        input.put("sid", "JLLKY-NEPGJ-BQCOR-EIJGY");
-        input.put("sign","true");
-
-		KeyInformation output = SKM.GetKeyInformationFromParameters(SKM.GetParameters(input, "Validate"));
-		
-		System.out.println(output.CreationDate());
-		
 	}
 
 }
