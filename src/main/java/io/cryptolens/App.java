e18c1d25d52dc641d46f2705ed6785623de3e4d2
Artem Los <arteml@kth.se>
2019-01-28 10:13:51 +0100

    Allow direct import of public key string into the library

--- src/main/java/io/cryptolens/App.java
+++ src/main/java/io/cryptolens/App.java
@@ -5,38 +5,40 @@ package io.cryptolens;
  */
 public class App 
 {
-  public static void main(String[] argv) {
-    Cryptolens cryptolens = Cryptolens.getDefault();
-    cryptolens.setExponentBase64("AQAB");
-    cryptolens.setModulusBase64("khbyu3/vAEBHi339fTuo2nUaQgSTBj0jvpt5xnLTTF35FLkGI+5Z3wiKfnvQiCLf+5s4r8JB/Uic/i6/iNjPMILlFeE0N6XZ+2pkgwRkfMOcx6eoewypTPUoPpzuAINJxJRpHym3V6ZJZ1UfYvzRcQBD/lBeAYrvhpCwukQMkGushKsOS6U+d+2C9ZNeP+U+uwuv/xu8YBCBAgGb8YdNojcGzM4SbCtwvJ0fuOfmCWZvUoiumfE4x7rAhp1pa9OEbUe0a5HL+1v7+JLBgkNZ7Z2biiHaM6za7GjHCXU8rojatEQER+MpgDuQV3ZPx8RKRdiJgPnz9ApBHFYDHLDzDw==");
+    public static void main(String[] argv) {
 
-    Cryptolens.ActivateResponse cryptolensResponse =
-        cryptolens.activate( "WyI0NjUiLCJBWTBGTlQwZm9WV0FyVnZzMEV1Mm9LOHJmRDZ1SjF0Vk52WTU0VzB2Il0="
-                           , 3646
-                           , "MPDWY-PQAOW-FKSCH-SGAAU"
-                           , "289jf2afs3"
-                           );
+        String RSAPubKey = "<RSAKeyValue><Modulus>khbyu3/vAEBHi339fTuo2nUaQgSTBj0jvpt5xnLTTF35FLkGI+5Z3wiKfnvQiCLf+5s4r8JB/Uic/i6/iNjPMILlFeE0N6XZ+2pkgwRkfMOcx6eoewypTPUoPpzuAINJxJRpHym3V6ZJZ1UfYvzRcQBD/lBeAYrvhpCwukQMkGushKsOS6U+d+2C9ZNeP+U+uwuv/xu8YBCBAgGb8YdNojcGzM4SbCtwvJ0fuOfmCWZvUoiumfE4x7rAhp1pa9OEbUe0a5HL+1v7+JLBgkNZ7Z2biiHaM6za7GjHCXU8rojatEQER+MpgDuQV3ZPx8RKRdiJgPnz9ApBHFYDHLDzDw==</Modulus><Exponent>AQAB</Exponent></RSAKeyValue>";
 
-    if (!cryptolensResponse.successful()) {
-      System.out.println("Failed to activate!");
-      Cryptolens.ActivateServerError er = cryptolensResponse.getServerError();
-      Exception ex = cryptolensResponse.getException();
+        Cryptolens cryptolens = Cryptolens.getDefault();
+        cryptolens.setRSAPubKey(RSAPubKey);
 
-      if (er != null) {
-        System.out.println("Server error: " + er);
-      }
+        Cryptolens.ActivateResponse cryptolensResponse =
+            cryptolens.activate( "WyI0NjUiLCJBWTBGTlQwZm9WV0FyVnZzMEV1Mm9LOHJmRDZ1SjF0Vk52WTU0VzB2Il0="
+                            , 3646
+                            , "MPDWY-PQAOW-FKSCH-SGAAU"
+                            , "289jf2afs3"
+                            );
 
-      if (ex != null) {
-        ex.printStackTrace(System.out);
-      }
+        if (!cryptolensResponse.successful()) {
+            System.out.println("Failed to activate!");
+            Cryptolens.ActivateServerError er = cryptolensResponse.getServerError();
+            Exception ex = cryptolensResponse.getException();
 
-      return;
+            if (er != null) {
+                System.out.println("Server error: " + er);
+            }
+
+            if (ex != null) {
+                ex.printStackTrace(System.out);
+            }
+
+            return;
+        }
+
+        LicenseKey licenseKey = cryptolensResponse.getLicenseKey();
+
+        System.out.println("Activation was successful!");
+        System.out.println(licenseKey.getKey());
+        System.out.println(licenseKey.getF1());
     }
-
-    LicenseKey licenseKey = cryptolensResponse.getLicenseKey();
-
-    System.out.println("Activation was successful!");
-    System.out.println(licenseKey.getKey());
-    System.out.println(licenseKey.getF1());
-  }
 }
