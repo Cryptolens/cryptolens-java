4e94a34c667ec6b847221c7d359cb50a9080f06c
Artem Los <arteml@kth.se>
2019-02-04 09:58:01 +0100

    Finish deactivate

--- src/main/java/io/cryptolens/methods/Key.java
+++ src/main/java/io/cryptolens/methods/Key.java
@@ -1,8 +1,10 @@
 package io.cryptolens.methods;
 
+import io.cryptolens.internal.BasicResult;
 import io.cryptolens.internal.HelperMethods;
 import io.cryptolens.models.ActivateModel;
 import io.cryptolens.internal.ActivateResult;
+import io.cryptolens.models.DeactivateModel;
 import io.cryptolens.models.LicenseKey;
 
 import java.util.HashMap;
@@ -16,10 +18,10 @@ public class Key {
 
     /**
      * Calls the Activate method (https://app.cryptolens.io/docs/api/v3/Activate).
-     * @param token
-     * @param RSAPubKey
-     * @param model
-     * @return
+     * @param token The access token with 'Activate' permission.
+     * @param RSAPubKey Your RSA Public Key, which can be found at https://app.cryptolens.io/docs/api/v3/QuickStart.
+     * @param model Method parameters.
+     * @return A LicenseKey object if success and null otherwise.
      */
     public static LicenseKey Activate (String token, String RSAPubKey, ActivateModel model) {
 
@@ -40,8 +42,27 @@ public class Key {
         return LicenseKey.LoadFromString(RSAPubKey, result.RawResponse);
     }
 
-    public static boolean Deactivate (String token) {
-        return false;
+    /**
+     * This method will 'undo' a key activation with a certain machine code.
+     * The key should not be blocked, since otherwise this method will throw an error.
+     * More info: https://app.cryptolens.io/docs/api/v3/Deactivate
+     * @param token The access token with 'Deactivate' permission.
+     * @param model Method parameters.
+     * @return True if deactivation succeeded and false otherwise.
+     */
+    public static boolean Deactivate (String token, DeactivateModel model) {
+
+        Map<String,String> extraParams = new HashMap<>();
+        extraParams.put("token", token);
+
+        BasicResult result = HelperMethods.SendRequestToWebAPI("key/deactivate", model, extraParams, BasicResult.class);
+
+        if(result.result == 1) {
+            System.err.println("The server returned an error: " + result.message);
+            return false;
+        }
+
+        return true;
     }
 
 }
