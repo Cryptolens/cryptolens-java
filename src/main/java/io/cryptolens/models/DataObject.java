0b4c5505737d07c23f41927e54936e032eb422dd
Artem Los <arteml@kth.se>
2019-03-01 18:18:51 +0100

    Update docs for Data object

--- src/main/java/io/cryptolens/models/DataObject.java
+++ src/main/java/io/cryptolens/models/DataObject.java
@@ -3,12 +3,27 @@ package io.cryptolens.models;
 import com.google.gson.annotations.SerializedName;
 
 public class DataObject {
+    /**
+     * The unique identifier of the data object.
+     */
     @SerializedName(value = "id", alternate = {"Id"})
     public int Id;
+
+    /**
+     * The name of the data object (max 10 chars).
+     */
     @SerializedName(value = "name", alternate = {"Name"})
     public String Name;
+
+    /**
+     * A string value associated with the data object (max 10,000 chars).
+     */
     @SerializedName(value = "stringValue", alternate = {"StringValue"})
     public String StringValue;
+
+    /**
+     * An int32 value associated with the data object.
+     */
     @SerializedName(value = "intValue", alternate = {"IntValue"})
     public int IntValue;
 }
