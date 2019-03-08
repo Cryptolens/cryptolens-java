bf4f6a544a05c7ca7f80046d99be288f922a8f3f
Artem Los <arteml@kth.se>
2019-03-08 14:35:11 +0100

    Start on machine code methods

---
+++ src/main/java/io/cryptolens/models/ProductKeyMachineCodeModel.java
@@ -0,0 +1,9 @@
+package io.cryptolens.models;
+
+public class ProductKeyMachineCodeModel extends ProductAndKeyModel {
+
+    /**
+     * The machine code (identifier of an activated device).
+     */
+    public String MachineCode;
+}
