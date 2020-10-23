dcf0368772d66cc5691a0d0778d092cf080cec0d
Artem Los <arteml@kth.se>
2020-10-23 10:27:59 +0200

    Update ActivateModel.java

--- src/main/java/io/cryptolens/models/ActivateModel.java
+++ src/main/java/io/cryptolens/models/ActivateModel.java
@@ -51,6 +51,12 @@ public class ActivateModel {
      * Allows you to specify a friendy name for the activated device, for example the employee's email.
      * Friendly name does not impact the number of active machine codes / seats, but it offers an easy way
      * of linking a machine/seat with a user. For added security, you can HMAC hash this value.
+     * <br/>
+     * The computer name can, as an example, be used as the friendly name:
+     * <code>
+     *     String friendlyName = InetAddress.getLocalHost().getHostName();
+     * </code>
+     *
      */
     public String FriendlyName;
 
