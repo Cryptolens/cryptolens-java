899c80113009995e816b59cb5a5a2ac82068d66d
Artem Los <arteml@kth.se>
2021-10-15 14:33:52 +0200

    Fix for license server (floating licenses)

--- src/main/java/io/cryptolens/internal/ActivateResult.java
+++ src/main/java/io/cryptolens/internal/ActivateResult.java
@@ -1,7 +1,12 @@
 package io.cryptolens.internal;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ActivateResult extends BasicResult {
 
+    @SerializedName(value = "licenseKey", alternate = {"LicenseKey"})
     public String licenseKey;
+
+    @SerializedName(value = "signature", alternate = {"Signature"})
     public String signature;
 }
--- src/main/java/io/cryptolens/internal/BasicResult.java
+++ src/main/java/io/cryptolens/internal/BasicResult.java
@@ -1,7 +1,15 @@
 package io.cryptolens.internal;
 
+import com.google.gson.annotations.SerializedName;
+
 public class BasicResult {
+
+    @SerializedName(value = "result", alternate = {"Result"})
     public int result;
+
+    @SerializedName(value = "message", alternate = {"Message"})
     public String message;
+
+    @SerializedName(value = "rawResponse", alternate = {"RawResponse"})
     public String RawResponse;
 }
