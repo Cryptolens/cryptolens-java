39d4db8591484ad06625f0170dee1daa33f940b1
Artem Los <arteml@kth.se>
2021-10-22 12:04:47 +0200

    Add GetKey overload without error parameter

--- src/main/java/io/cryptolens/methods/Key.java
+++ src/main/java/io/cryptolens/methods/Key.java
@@ -29,44 +29,45 @@ import java.util.Map;
  *
  * &lt;application ....
  * </pre>
- *
  */
 public class Key {
 
     /**
      * Calls the Activate method (https://app.cryptolens.io/docs/api/v3/Activate).
-     * @param token The access token with 'Activate' permission.
+     *
+     * @param token     The access token with 'Activate' permission.
      * @param RSAPubKey Your RSA Public Key, which can be found at https://app.cryptolens.io/docs/api/v3/QuickStart.
-     * @param model Method parameters.
+     * @param model     Method parameters.
      * @return A LicenseKey object if success and null otherwise.
      */
-    public static LicenseKey Activate (String token, String RSAPubKey, ActivateModel model) {
-        return Activate(token, RSAPubKey, model,null);
+    public static LicenseKey Activate(String token, String RSAPubKey, ActivateModel model) {
+        return Activate(token, RSAPubKey, model, null);
     }
 
     /**
      * Calls the Activate method (https://app.cryptolens.io/docs/api/v3/Activate?modelVersion=3).
      * <p>This method allows you to retrieve the error message from the Web API.</p>
      * <p>
-     *     To retrieve the error message, you need to initialize an APIError object and pass it in into the
-     *     "error" parameter. For example,
+     * To retrieve the error message, you need to initialize an APIError object and pass it in into the
+     * "error" parameter. For example,
      * </p>
      * <code>
-     *     APIError error = new APIError();<br>
-     *     LicenseKey license = Key.Activate(auth, RSAPubKey, new ActivateModel(3349, "ICVLD-VVSZR-ZTICT-YKGXL", Helpers.GetMachineCode()), error);<br>
-     *     System.out.println(error.message);
+     * APIError error = new APIError();<br>
+     * LicenseKey license = Key.Activate(auth, RSAPubKey, new ActivateModel(3349, "ICVLD-VVSZR-ZTICT-YKGXL", Helpers.GetMachineCode()), error);<br>
+     * System.out.println(error.message);
      * </code>
-     * @param token The access token with 'Activate' permission.
+     *
+     * @param token     The access token with 'Activate' permission.
      * @param RSAPubKey Your RSA Public Key, which can be found at https://app.cryptolens.io/docs/api/v3/QuickStart.
-     * @param model Method parameters.
-     * @param error The error object whose Message field will be populated if an error has occurred. Please initialize
-     *              this parameter, i.e. define <code>APIError error = new APIError();</code> and then pass
-     *              <code>error</code> into this parameter.
+     * @param model     Method parameters.
+     * @param error     The error object whose Message field will be populated if an error has occurred. Please initialize
+     *                  this parameter, i.e. define <code>APIError error = new APIError();</code> and then pass
+     *                  <code>error</code> into this parameter.
      * @return A LicenseKey object if success and null otherwise.
      */
-    public static LicenseKey Activate (String token, String RSAPubKey, ActivateModel model, APIError error) {
+    public static LicenseKey Activate(String token, String RSAPubKey, ActivateModel model, APIError error) {
 
-        Map<String,String> extraParams = new HashMap<>();
+        Map<String, String> extraParams = new HashMap<>();
 
         // force sign and the new protocol (only in activate)
         // modelVersion=3 docs: https://app.cryptolens.io/docs/api/v3/Activate?modelVersion=3
@@ -77,8 +78,8 @@ public class Key {
 
         ActivateResult result = HelperMethods.SendRequestToWebAPI("key/activate", model, extraParams, ActivateResult.class, error);
 
-        if(result == null || result.result == 1) {
-            if(result != null) {
+        if (result == null || result.result == 1) {
+            if (result != null) {
                 if (error != null) {
                     error.message = result.message;
                     error.errorType = ErrorType.WebAPIError;
@@ -98,25 +99,48 @@ public class Key {
      * Calls the GetKey method (https://app.cryptolens.io/docs/api/v3/GetKey?modelVersion=3).
      * <p>This method allows you to retrieve the error message from the Web API.</p>
      * <p>
-     *     To retrieve the error message, you need to initialize an APIError object and pass it in into the
-     *     "error" parameter. For example,
+     * To retrieve the error message, you need to initialize an APIError object and pass it in into the
+     * "error" parameter. For example,
      * </p>
      * <code>
-     *     APIError error = new APIError();<br>
-     *     LicenseKey license = Key.GetKey(auth, RSAPubKey, new GetKeyModel(3349, "ICVLD-VVSZR-ZTICT-YKGXL"), error);<br>
-     *     System.out.println(error.message);
+     * APIError error = new APIError();<br>
+     * LicenseKey license = Key.GetKey(auth, RSAPubKey, new GetKeyModel(3349, "ICVLD-VVSZR-ZTICT-YKGXL"), error);<br>
+     * System.out.println(error.message);
      * </code>
-     * @param token The access token with 'GetKey' permission.
+     *
+     * @param token     The access token with 'GetKey' permission.
      * @param RSAPubKey Your RSA Public Key, which can be found at https://app.cryptolens.io/docs/api/v3/QuickStart.
-     * @param model Method parameters.
-     * @param error The error object whose Message field will be populated if an error has occurred. Please initialize
-     *              this parameter, i.e. define <code>APIError error = new APIError();</code> and then pass
-     *              <code>error</code> into this parameter.
+     * @param model     Method parameters.
      * @return A LicenseKey object if success and null otherwise.
      */
-    public static LicenseKey GetKey (String token, String RSAPubKey, GetKeyModel model, APIError error) {
+    public static LicenseKey GetKey(String token, String RSAPubKey, GetKeyModel model) {
+        return GetKey(token, RSAPubKey, model, null);
+    }
 
-        Map<String,String> extraParams = new HashMap<>();
+    /**
+     * Calls the GetKey method (https://app.cryptolens.io/docs/api/v3/GetKey?modelVersion=3).
+     * <p>This method allows you to retrieve the error message from the Web API.</p>
+     * <p>
+     * To retrieve the error message, you need to initialize an APIError object and pass it in into the
+     * "error" parameter. For example,
+     * </p>
+     * <code>
+     * APIError error = new APIError();<br>
+     * LicenseKey license = Key.GetKey(auth, RSAPubKey, new GetKeyModel(3349, "ICVLD-VVSZR-ZTICT-YKGXL"), error);<br>
+     * System.out.println(error.message);
+     * </code>
+     *
+     * @param token     The access token with 'GetKey' permission.
+     * @param RSAPubKey Your RSA Public Key, which can be found at https://app.cryptolens.io/docs/api/v3/QuickStart.
+     * @param model     Method parameters.
+     * @param error     The error object whose Message field will be populated if an error has occurred. Please initialize
+     *                  this parameter, i.e. define <code>APIError error = new APIError();</code> and then pass
+     *                  <code>error</code> into this parameter.
+     * @return A LicenseKey object if success and null otherwise.
+     */
+    public static LicenseKey GetKey(String token, String RSAPubKey, GetKeyModel model, APIError error) {
+
+        Map<String, String> extraParams = new HashMap<>();
 
         // force sign and the new protocol (only in activate)
         // modelVersion=3 docs: https://app.cryptolens.io/docs/api/v3/Activate?modelVersion=3
@@ -127,8 +151,8 @@ public class Key {
 
         ActivateResult result = HelperMethods.SendRequestToWebAPI("key/getkey", model, extraParams, ActivateResult.class, error);
 
-        if(result == null || result.result == 1) {
-            if(result != null) {
+        if (result == null || result.result == 1) {
+            if (result != null) {
                 if (error != null) {
                     error.message = result.message;
                     error.errorType = ErrorType.WebAPIError;
@@ -148,11 +172,12 @@ public class Key {
      * This method will 'undo' a key activation with a certain machine code.
      * The key should not be blocked, since otherwise this method will throw an error.
      * More info: https://app.cryptolens.io/docs/api/v3/Deactivate
+     *
      * @param token The access token with 'Deactivate' permission.
      * @param model Method parameters.
      * @return True if deactivation succeeded and false otherwise.
      */
-    public static boolean Deactivate (String token, DeactivateModel model) {
+    public static boolean Deactivate(String token, DeactivateModel model) {
         return Deactivate(token, model, null);
     }
 
@@ -160,6 +185,7 @@ public class Key {
      * This method will 'undo' a key activation with a certain machine code.
      * The key should not be blocked, since otherwise this method will throw an error.
      * More info: https://app.cryptolens.io/docs/api/v3/Deactivate
+     *
      * @param token The access token with 'Deactivate' permission.
      * @param model Method parameters.
      * @param error The error object whose Message field will be populated if an error has occurred. Please initialize
@@ -167,15 +193,15 @@ public class Key {
      *              <code>error</code> into this parameter.
      * @return True if deactivation succeeded and false otherwise.
      */
-    public static boolean Deactivate (String token, DeactivateModel model, APIError error) {
+    public static boolean Deactivate(String token, DeactivateModel model, APIError error) {
 
-        Map<String,String> extraParams = new HashMap<>();
+        Map<String, String> extraParams = new HashMap<>();
         extraParams.put("token", token);
 
         BasicResult result = HelperMethods.SendRequestToWebAPI("key/deactivate", model, extraParams, BasicResult.class, error);
 
-        if(result == null || result.result == 1) {
-            if(result != null) {
+        if (result == null || result.result == 1) {
+            if (result != null) {
                 if (error != null) {
                     error.message = result.message;
                     error.errorType = ErrorType.WebAPIError;
@@ -201,6 +227,7 @@ public class Key {
      * <p>If a trial key was already created for a certain machine code, this method will try to find the license
      * key and return it instead. However, this will only occur if the license key is still a trial key (based
      * on feature definitions) and is not blocked.</p>
+     *
      * @param token The access token with 'CreateTrialKey' permission.
      * @param model Method parameters.
      * @return A new license key or error message.
@@ -219,6 +246,7 @@ public class Key {
      * <p>If a trial key was already created for a certain machine code, this method will try to find the license
      * key and return it instead. However, this will only occur if the license key is still a trial key (based
      * on feature definitions) and is not blocked.</p>
+     *
      * @param token The access token with 'CreateTrialKey' permission.
      * @param model Method parameters.
      * @param error The error object whose Message field will be populated if an error has occurred. Please initialize
@@ -227,7 +255,7 @@ public class Key {
      * @return A new license key or error message.
      */
     public static CreateKeyResult CreateTrialKey(String token, CreateTrialKeyModel model, APIError error) {
-        Map<String,String> extraParams = new HashMap<>();
+        Map<String, String> extraParams = new HashMap<>();
         extraParams.put("token", token);
 
         return HelperMethods.SendRequestToWebAPI("key/createtrialkey", model, extraParams, CreateKeyResult.class, error);
